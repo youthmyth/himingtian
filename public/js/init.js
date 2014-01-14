34de8961bca690deba0d2cac8c4e342ed090ff57
youthmyth <yominxixi@gmail.com>
2014-01-14 20:55:04 +0800

    更新css和note.html
    css文件增加init模块基础定义，添加label-dream模块，修改note，修改note.html的对应内容

--- public/js/init.js
+++ public/js/init.js
@@ -2,7 +2,8 @@ require.config({
   baseUrl:'/js',
   paths:{
     'angular':'../lib/angular',
-    'angular-bootstrap':'../lib/ui-bootstrap-tpls'
+    'angular-bootstrap':'../lib/ui-bootstrap-tpls',
+    'angular-animate':'../lib/angular-animate',
   },
   shim:{
     angular:{
@@ -11,14 +12,75 @@ require.config({
     'angular-bootstrap':{
       deps:['angular'] 
     },
+    'angular-animate':{
+      deps:['angular'] 
+    }
   }
 })
 define([
   'angular',
   'app',
   'controller/notes',
-  'directive/errors'
+  'directive/errors',
+  'directive/miniCalendar'
 ],function(angular){
   angular.bootstrap(document, ['himingtian']);
   document.body.setAttribute('ng-app', 'himingtian');
 })
+
+/*
+ * For shit IE
+ */
+if (!Array.prototype.forEach)
+{
+  Array.prototype.forEach = function(fun /*, thisArg */)
+  {
+    "use strict";
+
+    if (this === void 0 || this === null)
+      throw new TypeError();
+
+    var t = Object(this);
+    var len = t.length >>> 0;
+    if (typeof fun !== "function")
+      throw new TypeError();
+
+    var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+    for (var i = 0; i < len; i++)
+    {
+      if (i in t)
+        fun.call(thisArg, t[i], i, t);
+    }
+  };
+}
+
+if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function (searchElement, fromIndex) {
+      if ( this === undefined || this === null ) {
+        throw new TypeError( '"this" is null or not defined' );
+      }
+
+      var length = this.length >>> 0; // Hack to convert object.length to a UInt32
+
+      fromIndex = +fromIndex || 0;
+
+      if (Math.abs(fromIndex) === Infinity) {
+        fromIndex = 0;
+      }
+
+      if (fromIndex < 0) {
+        fromIndex += length;
+        if (fromIndex < 0) {
+          fromIndex = 0;
+        }
+      }
+
+      for (;fromIndex < length; fromIndex++) {
+        if (this[fromIndex] === searchElement) {
+          return fromIndex;
+        }
+      }
+
+      return -1;
+    };
+  }
